81f5b277bdeb9b29b85451da6015c4c1920a0740
Ali Gates <hacktech877@gmail.com>
2025-09-08 15:19:02 +0500

    iss

--- src/App.jsx
+++ src/App.jsx
@@ -41,9 +41,9 @@ function AppContent() {
           <Route
             path="/admin/*"
             element={
-              // <ProtectedRoute>
+              <ProtectedRoute>
               <AdminLayout />
-              // </ProtectedRoute>
+              </ProtectedRoute>
             }
           >
             <Route index element={<AdminDashboard />} />
